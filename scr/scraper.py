8e6b1a4f57893ad60c1d956d37f48358c6108bdc
mjbarca <72578816+mjbarca@users.noreply.github.com>
2020-10-21 00:28:03 +0200

    Update scraper.py
    He fusilado tu codigo de la API del INE pero he buscado la consulta de compraventa de viviendas. Los datos están por meses y me parecían más completos.
    Los he añadido a la tabla original mediante merge "left" que mantiene los datos del df left y cuando coinciden los del right los añade.
    
    Si quieres lo discutimos. Me parece que así sale bastante completo. Sólo está añadiendo el dato de viviendas vendidas con una .0 al final aunqe se trata de un int porque cuando crea la columna la crea con NaN y en ese momento crea la columna como float.

--- scr/scraper.py
+++ scr/scraper.py
@@ -13,6 +13,7 @@ import re;
 import os
 from datetime import date
 
+
 # this function converts month in strings into numbers. Month on original dataset are in spanish and english 
 
 def month_st2nu(month):
@@ -21,6 +22,50 @@ def month_st2nu(month):
     }
     out = months[month]
     return out
+
+
+# getting data from INE API
+
+dfINE = []  # defining work variables
+
+rowsINE = [] # here we will put finish result
+
+# INE API
+
+# compraventa de viviendas. ETDP1826
+
+url_plantilla = 'http://servicios.ine.es/wstempus/js/ES/DATOS_SERIE/{codigo}?nult={num_datos}'
+
+codigo = "ETDP1826"
+num_datos = 200
+
+url = url_plantilla.format(codigo=codigo, num_datos=num_datos)
+
+respuesta = requests.get(url)
+
+datos = respuesta.json()
+
+for x in datos['Data']:
+    
+    viviendas = x['Valor']
+    mes = x['FK_Periodo']
+    ano = x['Anyo']
+    
+    rowINE = {
+           'viviendas': int(viviendas),
+           'mes': int(mes),
+           'ano': int(ano)
+           }
+    
+    rowsINE.append(rowINE)
+    
+dfINE = pd.DataFrame(rowsINE) 
+
+
+#print(dfINE)
+
+
+# Getting now reference Indexes from ahe.es
 
 # source url
 
@@ -35,96 +80,160 @@ status = page.status_code
 if status == 200 :
 
   
-  soup = BeautifulSoup(page.content, "html5lib");   # https://beautiful-soup-4.readthedocs.io/en/latest/ 
+    soup = BeautifulSoup(page.content, "html5lib");   # https://beautiful-soup-4.readthedocs.io/en/latest/ 
   
-  table_data = soup.findAll("table")[1]
+    table_data = soup.findAll("table")[1]
   
-  df = []  # defining work variables
-  rows = [] # here we will put finish result
+    dfIndex = []  # defining work variables
+  
+    rowsIndex = [] # here we will put finish result
 
-  currentIndex=12 # avoid rowspan 
-  for row in table_data.findAll("tr"):
-     columns = row.findAll("td")
-     #print (len(columns))
-     if currentIndex%12 == 0: #
-      ano = (columns[0].text)  # Year
-      mes =  (columns[1].text)  # Month
-      tentesp = (columns[2].text)  # T.ENT. ESPAÑA
-      zeuro = (columns[3].text)  # ZONA EURO
-      euribor = (columns[4].text)  # EURIBOR
-      mibor = (columns[5].text)  # Mibor
-      irs = (columns[6].text)  # IRS 5 años
-      deuda = (columns[7].text)  # Deuda
-      bancos = (columns[8].text)  # Bancos
-      cajas = (columns[9].text)  # Cajas
-      ceca = (columns[10].text) # CECA 
-      ahe = (columns[11].text) # AHE
-      ced = (columns[12].text) # Ced
-     elif currentIndex%12 == 1:
-      #print (columns[0].text) # avoiding rowspan
-      mes =   (columns[1].text)  # month
-      tentesp = (columns[2].text)  # T.ENT. ESPAÑA
-      zeuro = (columns[3].text)  # ZONA EURO
-      euribor = (columns[4].text)  # EURIBOR
-      mibor = (columns[5].text)  # Mibor
-      irs = (columns[6].text)  # IRS 5 años
-      deuda = (columns[7].text)  # Deuda
-      bancos = (columns[8].text)  # Bancos
-      cajas = (columns[9].text)  # Cajas
-      ceca = (columns[10].text) # CECA 
-      ahe = (columns[11].text) # AHE
-      ced = (columns[12].text) # Ced    
-     else:
-      mes =   (columns[0].text)  # month
-      tentesp = (columns[1].text)  # T.ENT. ESPAÑA
-      zeuro = (columns[2].text)  # ZONA EURO
-      euribor = (columns[3].text)  # EURIBOR
-      mibor = (columns[4].text)  # Mibor
-      irs = (columns[5].text)  # IRS 5 años
-      deuda = (columns[6].text)  # Deuda
-      bancos = (columns[7].text)  # Bancos
-      cajas = (columns[8].text)  # Cajas
-      ceca = (columns[9].text) # CECA 
-      ahe = (columns[10].text) # AHE
-      ced = (columns[11].text) # Ced 
-      #print (columns[12].text) # solo hay 12 columnas  
-     currentIndex = currentIndex + 1
+    currentIndex=12 # avoid rowspan 
+  
+    for x in table_data.findAll("tr"):
+        
+        columns = x.findAll("td")
+        #print (len(columns))
+        if currentIndex%12 == 0: #
+            
+            ano = (columns[0].text)  # Year
+            
+            mes =  (columns[1].text)  # Month
+            
+            tentesp = (columns[2].text)  # T.ENT. ESPAÑA
+            
+            zeuro = (columns[3].text)  # ZONA EURO
+            
+            euribor = (columns[4].text)  # EURIBOR
+            
+            mibor = (columns[5].text)  # Mibor
+            
+            irs = (columns[6].text)  # IRS 5 años
+            
+            deuda = (columns[7].text)  # Deuda
+            
+            bancos = (columns[8].text)  # Bancos
+            
+            cajas = (columns[9].text)  # Cajas
+            
+            ceca = (columns[10].text) # CECA 
+            
+            ahe = (columns[11].text) # AHE
+                        
+            ced = (columns[12].text) # Ced
+        
+        elif currentIndex%12 == 1:
+      
+            #print (columns[0].text) # avoiding rowspan
+            
+            mes =   (columns[1].text)  # month
+            
+            tentesp = (columns[2].text)  # T.ENT. ESPAÑA
+            
+            zeuro = (columns[3].text)  # ZONA EURO
+            
+            euribor = (columns[4].text)  # EURIBOR
+            
+            mibor = (columns[5].text)  # Mibor
+            
+            irs = (columns[6].text)  # IRS 5 años
+            
+            deuda = (columns[7].text)  # Deuda
+            
+            bancos = (columns[8].text)  # Bancos
+            
+            cajas = (columns[9].text)  # Cajas
+            
+            ceca = (columns[10].text) # CECA 
+            
+            ahe = (columns[11].text) # AHE
+            
+            ced = (columns[12].text) # Ced    
+        
+        else:
+            
+            mes =   (columns[0].text)  # month
+            
+            tentesp = (columns[1].text)  # T.ENT. ESPAÑA
+            
+            zeuro = (columns[2].text)  # ZONA EURO
+            
+            euribor = (columns[3].text)  # EURIBOR
+            
+            mibor = (columns[4].text)  # Mibor
+            
+            irs = (columns[5].text)  # IRS 5 años
+            
+            deuda = (columns[6].text)  # Deuda
+            
+            bancos = (columns[7].text)  # Bancos
+            
+            cajas = (columns[8].text)  # Cajas
+            
+            ceca = (columns[9].text) # CECA 
+            
+            ahe = (columns[10].text) # AHE
+            
+            ced = (columns[11].text) # Ced 
+            
+            #print (columns[12].text) # solo hay 12 columnas  
+     
+        currentIndex = currentIndex + 1
 
-     # Formating exit row. Delete  \t, - & spaces. 
-     # nemrics we adopt english notation with "." instead ","
-     # https://docs.python.org/3/library/re.html    
-     # month is converting into numeric with ad-hoc function 
+        
+         # Formating exit row. Deleting  \t, - & spaces. 
+         # on numeric we adopt english notation with "." instead ","
+         # https://docs.python.org/3/library/re.html    
+         # month is converted into numeric with ad-hoc function 
+         # force conv to int in order to use as key in merging 
     
     
-     row = {'ano': re.sub("[\t -]", "", ano), 
-            'mes': month_st2nu(re.sub("[\t -]", "", mes)), 
-            'tentesp': re.sub("[,]", ".",re.sub("[\t -]", "", tentesp)),
-            'zeuro': re.sub("[,]", ".",re.sub("[\t -]", "", zeuro)),
-            'euribor':re.sub("[,]", ".",re.sub("[\t -]", "", euribor)),
-            'mibor':re.sub("[,]", ".",re.sub("[\t -]", "", mibor)),
-            'irs':re.sub("[,]", ".",re.sub("[\t -]", "", irs)),
-            'deuda':re.sub("[,]", ".",re.sub("[\t -]", "", deuda)),
-            'bancos':re.sub("[,]", ".",re.sub("[\t -]", "", bancos)),
-            'cajas':re.sub("[,]", ".",re.sub("[\t -]", "", cajas)),
-            'ceca':re.sub("[,]", ".",re.sub("[\t -]", "", ceca)),
-            'ahe':re.sub("[,]", ".",re.sub("[\t -]", "", ahe)),
-            'ced':re.sub("[,]", ".",re.sub("[\t -]", "", ced))}
+        rowIndex = {'ano': int(re.sub("[\t -]", "", ano)), 
+               'mes': int(month_st2nu(re.sub("[\t -]", "", mes))), 
+               'tentesp': re.sub("[,]", ".",re.sub("[\t -]", "", tentesp)),
+               'zeuro': re.sub("[,]", ".",re.sub("[\t -]", "", zeuro)),
+               'euribor':re.sub("[,]", ".",re.sub("[\t -]", "", euribor)),
+               'mibor':re.sub("[,]", ".",re.sub("[\t -]", "", mibor)),
+               'irs':re.sub("[,]", ".",re.sub("[\t -]", "", irs)),
+               'deuda':re.sub("[,]", ".",re.sub("[\t -]", "", deuda)),
+               'bancos':re.sub("[,]", ".",re.sub("[\t -]", "", bancos)),
+               'cajas':re.sub("[,]", ".",re.sub("[\t -]", "", cajas)),
+               'ceca':re.sub("[,]", ".",re.sub("[\t -]", "", ceca)),
+               'ahe':re.sub("[,]", ".",re.sub("[\t -]", "", ahe)),
+               'ced':re.sub("[,]", ".",re.sub("[\t -]", "", ced))}
    
-     rows.append(row)
+        rowsIndex.append(rowIndex)
   
-  df = pd.DataFrame(rows)
+    dfIndex = pd.DataFrame(rowsIndex)
+    
+    # merging the two dataframes by 'ano' & 'mes' because df
+    
+    
+    result = pd.merge(dfIndex, dfINE, how = 'left', on=['ano','mes'])  # merging by 'ano' + 'mes' https://pandas.pydata.org/pandas-docs/stable/user_guide/merging.html
+    
+    result = result.fillna('')  #taking out NaN. subs by ''
+        
+    # creating directory in case it is not existis df = df.fillna('')
+
+    if os.path.exists('csv') == False:
+        
+        os.mkdir('csv')
+    
+    # naming file with current date to save older files
+
+    today = date.today()
+    
+    todayTofile = today.strftime("%b-%d-%Y")   #https://www.programiz.com/python-programming/datetime/current-datetime
+    
+    # If SO is windows '../csv/historico_indices_'+todayTofile+'.csv'
+    # If SO is linux 'csv/historico_indices_'+todayTofile+'.csv'
+        
+    fileName = 'csv/historico_indices_'+todayTofile+'.csv'  
   
-  if os.path.exists('csv') == False:
-    os.mkdir('csv')
+    result.to_csv(fileName, index=False)
   
-  today = date.today()
-  todayTofile = today.strftime("%b-%d-%Y")   #https://www.programiz.com/python-programming/datetime/current-datetime
-  fileName = '../csv/historico_indices_'+todayTofile+'.csv'  
-  
-  df.to_csv(fileName, index=False)
-  
-  print ("Execution is finished.")
+    print ("Execution is finished.")
 
 else :
   
-  print('Conection to url has not been right!!')
+    print('Conection to url has not been right!!')
